c2cb1c8cb317876a1d16d5482f71f97ef9859c7d
anilgyawali <anilgyawali38@gmail.com>
2026-09-06 14:27:20 +0545

    voice page added

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   images: {
+  images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatar.iran.liara.run",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
     ],
   },
--- src/lib/actions/appointments.ts
+++ src/lib/actions/appointments.ts
@@ -1,6 +1,16 @@
 "use server";
-
+import { auth } from "@clerk/nextjs/server";
 import { prisma } from "@/lib/prisma";
+
+function transformAppointment(appointment: any) {
+  return {
+    ...appointment,
+    patientName: `${appointment.user.firstName || "Unknown"} ${appointment.user.lastName || "User"}`,
+    doctorName: appointment.doctor.name,
+    doctorImageUrl: appointment.doctor.imageUrl,
+    date: appointment.date.toISOString().split("T")[0],
+  };
+}
 
 export async function getAppointments() {
   try {
@@ -29,5 +39,61 @@ export async function getAppointments() {
   } catch (error) {
     console.error("Error fetching appointments:", error);
     throw new Error("Failed to fetch appointments");
+  }
+}
+
+export async function getUserAppointments() {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("you must be logg");
+    const user = await prisma.user.findUnique({ where: { clerkId: userId } });
+
+    if (!user) throw new Error("User not found");
+    const appointments = await prisma.appointment.findMany({
+      where: {
+        userId: user.id,
+      },
+      include: {
+        user: { select: { firstName: true, lastName: true, email: true } },
+        doctor: { select: { name: true, imageUrl: true } },
+      },
+      orderBy: [{ date: "desc" }, { time: "desc" }],
+    });
+    return appointments.map(transformAppointment);
+  } catch (errror) {
+    console.error("Error fetching user appointments:", errror);
+    throw new Error("Failed to fetch user appointments");
+  }
+}
+
+export async function getUserAppointmentStats() {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("You must be authenticated");
+
+    const user = await prisma.user.findUnique({ where: { clerkId: userId } });
+
+    if (!user) throw new Error("User not found");
+
+    // these calls will run in parallel, instead of waiting each other
+    const [totalCount, completedCount] = await Promise.all([
+      prisma.appointment.count({
+        where: { userId: user.id },
+      }),
+      prisma.appointment.count({
+        where: {
+          userId: user.id,
+          status: "COMPLETED",
+        },
+      }),
+    ]);
+
+    return {
+      totalAppointments: totalCount,
+      completedAppointments: completedCount,
+    };
+  } catch (error) {
+    console.error("Error fetching user appointment stats:", error);
+    return { totalAppointments: 0, completedAppointments: 0 };
   }
 }
