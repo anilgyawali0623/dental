eff0129b2f9a71c0367e9bc0bc4433baf5ac02f4
anilgyawali <anilgyawali38@gmail.com>
2026-09-06 14:38:21 +0545

    dashboard added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,19 @@
+import Navbar from "@/components/Navbar";
+import WelcomeSection from "@/components/dashboard/WelcomeSection";
 
-function Dashboard() {
+import MainActions from "@/components/dashboard/MainActions";
+import ActivityOverview from "@/components/dashboard/ActivityOverview";
+function DashboardPage() {
   return (
-    <div>
-       dashboard
-    </div>
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <MainActions />
+        <ActivityOverview />
+      </div>
+    </>
   )
 }
 
-export default Dashboard;
+export default DashboardPage;
